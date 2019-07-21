321ba345eeec1aedec263e4e1456de3f39f81386
Andres Gonzalez <andresg9108@gmail.com>
2019-07-21 14:35:58 -0500

    Implementing process-sql task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,18 @@
 'use strict';
 module.exports = function(grunt) {
+	grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        processpy: {
+            sql: [{
+                file: 'E:/sql/test/test.sql', 
+                folder: 'E:/sql/test/sql'
+            },{
+                file: 'E:/sql/test/test2.sql',
+                folder: 'E:/sql/test/sql2'
+            }]
+        }
+    });
+    
 	grunt.loadTasks('tasks');
 };
---
+++ tasks/lib/process_sql.js
@@ -0,0 +1,28 @@
+module.exports.init = function(grunt){
+	var exports = {};
+
+	exports.exec = function(done, aSQL){
+		for (var i=0; i<aSQL.length; i++) {
+			var oSQL = aSQL[i];
+			var sFile = oSQL.file;
+			var sFolder = oSQL.folder;
+			
+			var command, process, exec;
+		 
+			command = "python node_modules/processpy/process.py -sql "+sFile+" "+sFolder;
+			grunt.log.writeln('Run: ' + command);
+			exec = require('child_process').exec;
+			process = exec(command, function (error, stdout, stderr) {
+				if (error) {
+					if(error.code !== 0){
+						grunt.warn(stderr);
+						grunt.log.writeln(error.stack);
+					}
+				}
+				done();
+			});
+		}
+	};
+	 
+	return(exports);
+};
